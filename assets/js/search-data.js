2f97fb130813bac1009906a5d6e8c47bd5e20657
ftavora-argos <ftavora@gmail.com>
2026-04-12 22:24:18 +0000

    Deploying to gh-pages from @ ftavora-argos/fabiotavora.science@9597cdef1f025ba33aca2507edc6c9137b1537da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,7 +56,7 @@ ninja.data = [{
           description: "Current and former members of the Távora Research Group at the Federal University of Ceará Graduate Program in Pathology and Argos Laboratory.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/people/";
+            window.location.href = "/Research%20Group";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
